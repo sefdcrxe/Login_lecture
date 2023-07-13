cff7dc4958f5c230cc63b65f9401f67219e74fb8
sefdcrxe <128684050+sefdcrxe@users.noreply.github.com>
2023-07-14 03:32:37 +0900

    돔을 이용하여 html 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,12 +12,24 @@ function login(){
         psword : psword.ariaValueMax,
     };
 
+    //로그인 api : /auth/signin
+    //로그인 req 
     fetch("/login", {
         method : "POST",
         header : {
             "Content-Type" : "application/json",
-
         },
         body: JSON.stringify(req),
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            if (res.success){
+                location.href = "/";
+            } else {
+                alert(res.msg);
+            }
+        })
+        .catch((err) => {
+        console.error("로그인 중 에러 발생");
     });
 }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,17 +1,24 @@
 "use strict";
 
-
-const home = (req, res) => {
-    res.render("home/index");
+const output = {
+    home : (req, res) => {
+        res.render("home/index");
+    },
+    
+    login : (req, res) => {
+        res.render("home/login");
+    },
 };
 
-const login = (req, res) => {
-    res.render("home/login");
+const process = {
+    login : (req, res) => {
+        console.log(req.body);
+    },
 };
 
 //꼭 밖으로 내보내야 밖에서 사용할 수 있음
 module.exports = {
-    home,
-    login
+    output,
+    process
 };
 
